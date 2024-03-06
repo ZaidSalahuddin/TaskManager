69de94752661b057d2e527d190cba4ffe44639f0
ZaidSalahuddin <59754860+ZaidSalahuddin@users.noreply.github.com>
2024-03-06 00:03:49 -0500

    so the list names kinda works but it sa bit scuffed atm

--- taskmanager/main/urls.py
+++ taskmanager/main/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import formPageView, listNamesPageView
+from .views import formPageView, listNamesPageView, addNamePageView
 
 urlpatterns = [
     path("", formPageView, name="form"),
-    path("listnames", listNamesPageView, name="names")
+    path("listnames", listNamesPageView, name="names"),
+    path("addnames", addNamePageView, name="addname")
 ]
--- taskmanager/main/views.py
+++ taskmanager/main/views.py
@@ -6,6 +6,16 @@ def formPageView(request):
     return render(request, "form.html")
 
 def listNamesPageView(request) :
-	data = Name.objects.all()
-	context = {"names": data}
-	return render(request, "listnames.html", context)
+     
+    if request.method == 'POST':
+        new_vet = Name()
+        new_vet.first_name = request.POST.get("firstname")
+        new_vet.last_name = request.POST.get("lastname")
+        new_vet.save()
+    
+    data = Name.objects.all()
+    context = {"names": data}
+    return render(request, "listnames.html", context)
+
+def addNamePageView(request) :
+   return render(request, "addname.html")
