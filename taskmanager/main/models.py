297a08a43352731b57aa7a31c6d59020eea819ae
ZaidSalahuddin <59754860+ZaidSalahuddin@users.noreply.github.com>
2024-03-11 11:27:19 -0400

    idek man

--- taskmanager/main/models.py
+++ taskmanager/main/models.py
@@ -5,17 +5,17 @@ class Name(models.Model):
     first_name = models.CharField(max_length=30, default="John")
     last_name = models.CharField(max_length=30, default="Doe")
 #    email = models.EmailField(max_length=200, blank=True)
-    phone = models.CharField(max_length=15, blank=True)
+    column3 = models.CharField(max_length=15, blank=True)
 
     def __str__(self):
         return(self.first_name + " " + self.last_name)
     
 class Task(models.Model):
-    name = models.CharField(max_length=30, default='work')
-    description = models.CharField(max_length=30, default='help')
+    column = models.CharField(max_length=30, default='work')
+    column2 = models.CharField(max_length=30, default='help')
 #    due_date = models.DateTimeField(default="1/1/1970")
     time_to_complete = models.IntegerField(default=0, blank=True)
 #    assigned_worker = models.ManyToManyField(Name, blank=True)
 
     def __str__(self):
-          return (self.name)
+          return ("hello there") #self.name + "" + self.description + "" + self.time_to_complete)
--- taskmanager/main/urls.py
+++ taskmanager/main/urls.py
@@ -3,7 +3,7 @@ from .views import *
 
 urlpatterns = [
     path("", listTasksPageView, name="tasks"),
-    path("listtasks", listTasksPageView, name="tasks"),
+#    path("listtasks", listTasksPageView, name="tasks"),
     path("addnames", addTaskPageView, name="addtask"),
     path("editname/<int:iTaskID>", editTaskPageView, name="edittask"),
     path("deletetask/<int:iTaskID>", deleteTaskPageView, name="deletetask")
