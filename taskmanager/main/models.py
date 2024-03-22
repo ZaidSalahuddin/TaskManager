6848be87daba657e16163f9290499489d0dc703d
ZaidSalahuddin <59754860+ZaidSalahuddin@users.noreply.github.com>
2024-03-22 17:27:24 -0400

    changed the max length of descriptioon

--- taskmanager/main/models.py
+++ taskmanager/main/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 class Name(models.Model):
     task = models.CharField(max_length=30)
-    description = models.CharField(max_length=30)
+    description = models.CharField(max_length=100)
     time_to_complete = models.CharField(max_length=11)
 
 
