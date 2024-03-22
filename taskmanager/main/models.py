cfd1d8e83b5adc15ce603da85578facf8d972a68
ZaidSalahuddin <59754860+ZaidSalahuddin@users.noreply.github.com>
2024-03-22 13:43:18 -0400

    removed the uneeded model

--- taskmanager/main/models.py
+++ taskmanager/main/models.py
@@ -1,16 +1,6 @@
 from django.db import models
 
 # Create your models here.
-class Task(models.Model):
-	task_name = models.CharField(max_length=30)
-	worker_name = models.CharField(max_length=30)
-	task_description = models.CharField(max_length=30)
-	time_to_complete = models.IntegerField(default=0)
- 
-	def __str__(self):
-          return (self.task_name)
-
-
 class Name(models.Model):
     task = models.CharField(max_length=30)
     description = models.CharField(max_length=30)
